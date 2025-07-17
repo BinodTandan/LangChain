132963ecaf9385bfdf41d4a04f0e23f798d255ce
Binod Tandan <binodtandan26@gmail.com>
2025-07-17 17:12:28 -0500

    Importing prompt template as json

--- langchain_prompts/prompt_ui.py
+++ langchain_prompts/prompt_ui.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
 import streamlit as st
-from langchain_core.prompts import PromptTemplate,load_prompt
+from langchain_core.prompts import load_prompt
 
 
 load_dotenv()
@@ -15,29 +15,23 @@ style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "T
 
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
-template = PromptTemplate(
-    template = '''
-    Please summarize the research paper titled {paper_input} with the following specifications:
-    Explanation Style: {style_input}  
-    Explanation Length: {length_input}  
-    1. Mathematical Details:  
-    - Include relevant mathematical equations if present in the paper.  
-    - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
-    2. Analogies:  
-    - Use relatable analogies to simplify complex ideas.  
-    If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  \n
-    Ensure the summary is clear, accurate, and aligned with the provided style and length.
-    ''',
-    input_variable = ['paper_input', 'style_input', 'length_input']
-)
+template = load_prompt('template.json')
 
-prompt =template.invoke({
-    'paper_input': paper_input,
-    'style_input': style_input,
-    'length_input': length_input
-})
+# prompt =template.invoke({
+#     'paper_input': paper_input,
+#     'style_input': style_input,
+#     'length_input': length_input
+# })
 
 if st.button("Summarize"):
-    result = model.invoke(prompt)
+    chain = template | model
+    result = chain.invoke(
+        {
+            'paper_input': paper_input,
+            'style_input': style_input,
+            'length_input': length_input
+        }
+    )
+   
     st.write(result.content)
 
