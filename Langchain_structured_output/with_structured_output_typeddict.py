1a1e037ab6e48f9bf0b4c6f74393e1506ba4ca00
Binod Tandan <binodtandan26@gmail.com>
2025-07-23 19:23:07 -0500

    TypedDict with Annotated

--- Langchain_structured_output/with_structured_output_typeddict.py
+++ Langchain_structured_output/with_structured_output_typeddict.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from typing import TypedDict
+from typing import TypedDict, Annotated, Optional, Literal
 
 
 load_dotenv()
@@ -9,14 +9,27 @@ model = ChatOpenAI()
 
 #schema
 class Review(TypedDict):
-    summary: str
-    sentiment: str
+    key_themes: Annotated[list[str], "Write down the key themes discussed in the review in a list"]
+    summary: Annotated[str, "A brief summary of review"] 
+    sentiment: Annotated[Literal['pos', 'neg'], "Return sentiment of the review either negative, positive, or netural"] 
+    pros: Annotated[Optional[list[str]], "Write down all the pros inside the list"]
+    cons: Annotated[Optional[list[str]], "Write down all the cons inside the list"]
+    name: Annotated[Optional[str], "Name of the reviewer"]
 
 structured_model = model.with_structured_output(Review, method="function_calling")
 
-result = structured_model.invoke("""I find this movie one of my childhood favourites I was only 7 to 8 yrs old when it came out used to watch it loads on video back when modern 
-technology Youtube did not exist. On 100th anniversary mum and I watched it in 3d and recently watched it again together for 25th year since
-it was released it even went on my own to see it again as its just so epic I just feel like I could watch it over 100 times.""")
+result = structured_model.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+Review by Binod Tandan""")
 
 print(result)
 
