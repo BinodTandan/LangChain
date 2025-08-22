920a48241289ad205e9b1c7d2d9332b1483f5aca
Binod Tandan <binodtandan26@gmail.com>
2025-08-22 12:06:00 -0500

    update

--- langchain_prompts/chat_prompt_template.py
+++ langchain_prompts/chat_prompt_template.py
@@ -5,7 +5,6 @@ chat_template = ChatPromptTemplate([
     ('system', 'You are a helpful {domain} expert'),
     ('human', 'Explain in simple terms, what is {topic}')
 ])
-
 prompt = chat_template.invoke({
     'domain': "cricket",
     'topic' : "Chakka"
