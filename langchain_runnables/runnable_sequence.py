98915091f9a556c4d475d62990711f4dcf9ac5f1
Binod Tandan <binodtandan26@gmail.com>
2025-10-17 20:07:16 -0500

    new folder with runnableSequence

---
+++ langchain_runnables/runnable_sequence.py
@@ -0,0 +1,24 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}.',
+    input_variables= ['topic']
+)
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following - {text}',
+    input_variables= ['text']
+)
+chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+
+print(chain.invoke({'topic': 'AI'}))
